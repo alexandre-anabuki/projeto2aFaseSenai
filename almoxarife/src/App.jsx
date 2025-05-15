52fb53d5d3f7d828156faf27581d2d9f442c6479
alexandre-anabuki <alex.anabuki@gmail.com>
2025-05-14 22:22:57 -0300

    Criação do cadastro e login

---
+++ almoxarife/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Cadastro from './pages/Cadastro'
+
+function App() {
+
+  return (
+    <div className='container-app'>
+      <Cadastro />
+    </div>
+  )
+}
+
+export default App
---
+++ almoxarife/src/pages/Cadastro.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Cadastro.css'
+function Cadastro() {
+  return (
+    <div className='container-cadastro'>
+        <h2 className='titulo-cadastro'>Cadastro</h2>
+
+        <div className='contauiner-inputs'>
+            <div>
+                <label htmlFor="">Nome</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Senha</label>
+                <input type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Telefone</label>
+                <input type="text" />
+            </div>
+        </div>
+
+        <div className='container-botoes'>
+            <button className='btn-cadastra'>Cadastrar</button>
+            <button className='btn-cancela'>Cancelar</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Cadastro
