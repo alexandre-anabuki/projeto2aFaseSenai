f7a13465defb8baaa25b9563a1bf8fc00614f0bd
alexandre-anabuki <alex.anabuki@gmail.com>
2025-05-22 22:23:53 -0300

    atualização da semana

--- almoxarife/src/App.jsx
+++ almoxarife/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
+import Inventario from './pages/Inventario'
 
 function App() {
   // const [pagina, setPagina] = useState(<Login />)
 
   return (
     <div className='container-app'>
-      <Login />
+      <Inventario />
+      {/* <Login /> */}
       {/* <Cadastro /> */}
       {/* {pagina} */}
     </div>
---
+++ almoxarife/src/pages/Inventario.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Inventario.css'
+function Inventario() {
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Estoque</th>
+                    <th>N° Patrimônio</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+  )
+}
+
+export default Inventario
